7f4c3e1da1ec2a722c39088f7ec8275cb6127269
nathanye <khalil54781@gmail.com>
2017-07-17 15:48:13 +0800

    touch app/views/carts/checkout.html.erb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,4 +3,8 @@ class CartsController < ApplicationController
     current_cart.clean!
     redirect_to carts_path, warning: "已清空购物车"
   end
+
+  def checkout
+    @order = Order.new(order.params)
+  end
 end
